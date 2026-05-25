2be7d823a7c0ea96ab55a77965c9de557275e591
kang-samid <124869969+kang-samid@users.noreply.github.com>
2026-05-26 06:33:02 +0700

    Update botnews.py

--- botnews.py
+++ botnews.py
@@ -1,3 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+# Flask kecil untuk menipu Render agar mengira ini adalah web service
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Sedang Aktif"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
+
 import feedparser
 import requests
 import time
